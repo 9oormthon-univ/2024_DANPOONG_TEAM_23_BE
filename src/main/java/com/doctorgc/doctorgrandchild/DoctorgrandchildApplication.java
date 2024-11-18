57e92247a7f33a831181ed9de3571a452f80bfd1
minahkim03 <minahkim026@gmail.com>
2024-11-18 10:51:45 +0900

    first commit

---
+++ src/main/java/com/doctorgc/doctorgrandchild/DoctorgrandchildApplication.java
@@ -0,0 +1,13 @@
+package com.doctorgc.doctorgrandchild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorgrandchildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoctorgrandchildApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/doctorgc/doctorgrandchild/DoctorgrandchildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doctorgc.doctorgrandchild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorgrandchildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
